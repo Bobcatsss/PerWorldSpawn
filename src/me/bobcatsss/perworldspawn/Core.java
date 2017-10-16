db1fc50f409fa63c10b771f6a72bf545271b1e08
Aaron <Aaron@192.168.1.20>
2017-10-16 02:37:02 +0100

    Updated

---
+++ src/me/bobcatsss/perworldspawn/Core.java
@@ -0,0 +1,23 @@
+package me.bobcatsss.perworldspawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public void registerEvents() {
+		
+	}
+	
+	public void registerCommands() {
+		
+	}
+
+}
